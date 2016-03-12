fb67d5137502406be11a945dd04b734e04807ea1
Sebastián Badea <sebastian.badea@infojobs.net>
2016-03-12 18:33:46 +0100

    Initial Commit

---
+++ PlayingWithRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayingWithRxSwift
+//
+//  Created by Sebastián Badea on 12/3/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
