57b116a1b88175d85ddd7ac3a7c5534e8f7d0320
Sebastián Badea <sebastian.badea@infojobs.net>
2016-03-14 08:07:46 +0100

    Added playground generators page and a just generator example implementation

---
+++ PlayingWithRxSwiftPlayground.playground/Pages/Generators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+import RxSwift
+
+func myJust<E>(element: E) -> Observable<E> {
+    return Observable.create { observer in
+        observer.on(.Next(element))
+        observer.on(.Completed)
+        return NopDisposable.instance
+    }
+}
+
+myJust(0)
+    .subscribeNext { n in
+        print(n)
+}
