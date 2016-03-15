3bf99331db86725c3161e4b92ba2c9d395119789
Sebastián Badea <sebastian.badea@infojobs.net>
2016-03-15 07:32:00 +0100

    Added observable generator 1 page and example: observable that generates only one Int element and then completes

---
+++ PlayingWithRxSwiftPlayground.playground/Pages/Generators 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: [Previous](@previous)
+
+import Foundation
+import RxSwift
+
+func myJust<E>(element: E) -> Observable<E> {
+    return Observable.create { observer in
+        observer.on(.Next(element))
+        observer.on(.Completed)
+        return NopDisposable.instance
+    }
+}
+
+myJust(0)
+    .subscribeNext { n in
+        print(n)
+}
+
+myJust(0)
+    .subscribe { n in
+        print(n)
+}
